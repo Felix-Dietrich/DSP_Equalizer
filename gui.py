098838189d0e2d7a22a323b273f9b0848bef32eb
Felix Dietrich <figsu96@gmail.com>
2024-12-21 15:17:52 +0100

    audoi processing in background thread

--- gui.py
+++ gui.py
@@ -18,6 +18,27 @@ lib = ctypes.CDLL("./C_code/library.so")
 lib.filterfunction.argtypes = [ctypes.POINTER(ctypes.c_int16), ctypes.c_size_t]
 lib.filterfunction.restype = None
 
+
+def  hello_world():
+    lib.hello_world()
+
+
+def update_spectrum():
+    lib.update_spectrum()
+
+def open_file():
+    file_path = filedialog.askopenfilename(filetypes=[("Audio Files", "*.wav *.mp3")])
+    if file_path:
+        file_label.config(text=f"Selected file: {file_path}")
+
+def update_plot():
+    spectrum = [(0-slider.get()) for slider in sliders]
+    ax.clear()
+    ax.bar(range(1, 21), spectrum, color='blue')
+    ax.set_ylim(-20, 20)
+    canvas.draw()
+    
+
 def load_audio_to_ctypes(filepath):
     # Load the audio file using pydub
     audio = AudioSegment.from_file(filepath)
@@ -37,46 +58,40 @@ def load_audio_to_ctypes(filepath):
 
     return ctypes_array, len(audio_data), audio.frame_rate
 
+def audio_processing():
+    SAMPLES_PER_BUFFER = 4000
+    hello_world()
+    filepath = "./example.wav"
+    audiodata, size, samplerate = load_audio_to_ctypes(filepath)
+    update_spectrum()
+    # Audio stream initialisieren (pyaudio)
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16,
+                    channels=1,
+                    rate=samplerate,
+                    output=True,
+                    frames_per_buffer=SAMPLES_PER_BUFFER)  # Puffergröße auf die Größe eines Slices setzen
 
 
-def  hello_world():
-    lib.hello_world()
+    for i in range(0,size-SAMPLES_PER_BUFFER,SAMPLES_PER_BUFFER):
+        slice_data = audiodata[i:i+SAMPLES_PER_BUFFER]
+        SliceArrayType = ctypes.c_int16 * len(slice_data)
+        slice_array = SliceArrayType(*slice_data)
+        lib.filterfunction(slice_array,SAMPLES_PER_BUFFER)
+
+        # Umwandeln der verarbeiteten Daten in ein AudioSegment
+        # Pydub erwartet die Audiodaten als Bytes, daher müssen wir sie in ein passendes Format bringen
+        np_data = np.array(slice_array, dtype=np.int16)
+        audio_segment = np_data.tobytes()
+        # Audio-Daten streamen
+        stream.write(audio_segment)
+        #if i == 40*2000:
+        #   plt.plot(np_data)
+        #  plt.show()
 
 
 
 
-SAMPLES_PER_BUFFER = 4000
-   
-
-
-
-hello_world()
-filepath = "./example.wav"
-audiodata, size, samplerate = load_audio_to_ctypes(filepath)
-lib.update_spectrum()
-# Audio stream initialisieren (pyaudio)
-p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16,
-                channels=1,
-                rate=samplerate,
-                output=True,
-                frames_per_buffer=SAMPLES_PER_BUFFER)  # Puffergröße auf die Größe eines Slices setzen
-
-
-
-
-
-def open_file():
-    file_path = filedialog.askopenfilename(filetypes=[("Audio Files", "*.wav *.mp3")])
-    if file_path:
-        file_label.config(text=f"Selected file: {file_path}")
-
-def update_plot():
-    spectrum = [(0-slider.get()) for slider in sliders]
-    ax.clear()
-    ax.bar(range(1, 21), spectrum, color='blue')
-    ax.set_ylim(-20, 20)
-    canvas.draw()
 
 # Main window
 root = tk.Tk()
@@ -93,10 +108,10 @@ file_label.pack(side=tk.LEFT)
 # Equalizer sliders
 slider_frame = ttk.Frame(root)
 slider_frame.pack(pady=20)
-label = ttk.Label(slider_frame, text="")
+label = ttk.Label(slider_frame, text="+20dB\n\n\n0dB\n\n\n-20dB")
 label.grid(row=0, column=0, padx=2) 
 
-label = ttk.Label(slider_frame, text="freq(HZ)")
+label = ttk.Label(slider_frame, text="Freq(Hz)")
 label.grid(row=1, column=0, padx=2) 
 sliders = []
 for i in range(20):
@@ -117,27 +132,15 @@ canvas = FigureCanvasTkAgg(fig, master=root)
 canvas_widget = canvas.get_tk_widget()
 canvas_widget.pack()
 
+
+# Start the background task in a separate thread
+thread = threading.Thread(target=audio_processing, daemon=True)
+thread.start()
+
 # Run the GUI
 root.mainloop()
 
 
-
-
-for i in range(0,size-SAMPLES_PER_BUFFER,SAMPLES_PER_BUFFER):
-    slice_data = audiodata[i:i+SAMPLES_PER_BUFFER]
-    SliceArrayType = ctypes.c_int16 * len(slice_data)
-    slice_array = SliceArrayType(*slice_data)
-    lib.filterfunction(slice_array,SAMPLES_PER_BUFFER)
-
-     # Umwandeln der verarbeiteten Daten in ein AudioSegment
-    # Pydub erwartet die Audiodaten als Bytes, daher müssen wir sie in ein passendes Format bringen
-    np_data = np.array(slice_array, dtype=np.int16)
-    audio_segment = np_data.tobytes()
-    # Audio-Daten streamen
-    stream.write(audio_segment)
-    #if i == 40*2000:
-     #   plt.plot(np_data)
-      #  plt.show()
 
     
 
