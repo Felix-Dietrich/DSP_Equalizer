afde8d47e391a1f952a5e996590f23f9e06356cd
Felix Dietrich <figsu96@gmail.com>
2024-12-23 22:13:50 +0100

    implemented reconstruction filter

--- C_code/filter_dwt.c
+++ C_code/filter_dwt.c
@@ -24,58 +24,118 @@ static void filter_dwt(buffer_pcm_t* in, buffer_pcm_t* out, float volume)
 {
     dwt_coefficients_t dwt_coefficients;
     decomposition(in, &dwt_coefficients);
+    reconstruction(out,&dwt_coefficients);
 }
 
 static void decomposition(buffer_pcm_t* in, dwt_coefficients_t* coefficients)
 {
     int size = FILTERLENGTH;
-    int16_t detail[FILTERLENGTH>>1];
+    int16_t detail[FILTERLENGTH/2];
     int16_t* temp_p;
-    int16_t next_in[FILTERLENGTH>>1];
-    int16_t approximation[FILTERLENGTH>>1];
+    int16_t next_in[FILTERLENGTH/2];
+    int16_t approx[FILTERLENGTH/2];
     int16_t* next_in_p = next_in;
-    int16_t* approximation_p = approximation;
+    int16_t* approx_p = approx;
 
-    apply_wavelets(in, detail, approximation, size);
+    decompose_wavelets(in->data, detail, approx, size);
     size/=2;
-    for(int i = 0; i < 8;i++)
+    for(int i = 0; i < 16;i+=2)
     {
-        apply_wavelets(detail, coefficients[i],coefficients[i+1],size);
-        apply_wavelets(next_in_p, detail, approximation_p, size);
+        decompose_wavelets(detail, coefficients[i],coefficients[i+1],size);
+        decompose_wavelets(next_in_p, detail, approx_p, size);
         //swap pointers
         temp_p = next_in_p;
-        next_in_p = approximation_p;
-        approximation_p = temp_p;
+        next_in_p = approx_p;
+        approx_p = temp_p;
         size/=2;
     }
-    apply_wavelets(detail, coefficients[16],coefficients[17],size);
-    apply_wavelets(next_in_p, coefficients[18], coefficients[19], size);
+    decompose_wavelets(detail, coefficients[16],coefficients[17],size);
+    decompose_wavelets(next_in_p, coefficients[18], coefficients[19], size);
 }
 
-static void reconstruction()
-{
 
-}
 
-static void apply_wavelets(int16_t in[], int16_t detail[], int16_t approximation[],int size)
+
+static void reconstruction(buffer_pcm_t* out, dwt_coefficients_t* coefficients)
 {
-    for(int i = 0; i < size; i+=2)
+    int16_t detail[FILTERLENGTH/2];
+    int16_t approx[FILTERLENGTH/2];
+    int16_t next_out[FILTERLENGTH/2];
+    int16_t size = 2;
+    int16_t* next_out_p = next_out;
+    int16_t* approx_p = approx;
+    int16_t* temp_p;
+
+    reconstruct_wavelets(next_out_p,coefficients[18],coefficients[19],size);
+    reconstruct_wavelets(detail,coefficients[16],coefficients[17],size);
+
+    for(int i = 15; i>=1;i-=2)
     {
-        apply_wavelet(&in[i],&detail[i/2],&approximation[i/2]);
+        temp_p = next_out_p;
+        next_out_p = approx_p;
+        approx_p = temp_p;
+        size*=2;
+        reconstruct_wavelets(next_out_p,detail,approx_p,size);
+        reconstruct_wavelets(detail,coefficients[i-1],coefficients[i],size);
+    }
+    size*=2;
+    reconstruct_wavelets(out->data,detail,next_out_p,size);
+    out->size=size;
+}
+
+static void decompose_wavelets(int16_t in[], int16_t detail[], int16_t approx[],int size)
+{
+    for(int i = 0; i < size/2; i++)
+    {
+        decompose_wavelet(&in[i*2],&detail[i],&approx[i]);
     }
 
 }
 
-static void apply_wavelet(int16_t in[WAVELET_SIZE], int16_t* detail, int16_t* approximation)
+
+/**
+ *  Beislpiel mit haar wavelet
+ *   in  1    2
+ *   detail  0.5
+ *   approx  1.5
+ */
+static void decompose_wavelet(int16_t in[WAVELET_SIZE], int16_t* detail, int16_t* approx)
 {
-    *approximation = 0;
+    *approx = 0;
     *detail = 0;
     for(int i = 0; i < (WAVELET_SIZE-1); i+=2)
     {
-        *approximation += (in[i] * wavelet[i])/2;
-        *approximation += (in[i+1] * wavelet[i+1])/2;
-        *detail += (in[i]* wavelet[WAVELET_SIZE-1-i]);
-        *detail -= (in[i+1]* wavelet[WAVELET_SIZE-2-i]);
+        *approx += (in[i] * wavelet[i])/2;
+        *approx += (in[i+1] * wavelet[i+1])/2;
+        *detail += (in[i]* wavelet[WAVELET_SIZE-1-i])/2;
+        *detail -= (in[i+1]* wavelet[WAVELET_SIZE-2-i])/2;
     }
-  
 }
+
+
+static void reconstruct_wavelets(int16_t out[], int16_t detail[], int16_t approx[],int size)
+{
+    for(int i = 0; i < size/2; i++)
+    {
+        reconstruct_wavelet(out[i*2],&detail[i],&approx[i]);
+    }
+}
+
+/**
+ *  Beislpiel mit haar wavelet
+ *   detail  0.5 
+ *   approx  1.5
+ *   out  1    2 
+ */
+static void reconstruct_wavelet(int16_t out[WAVELET_SIZE], int16_t* detail, int16_t* approx)
+{
+    for(int i = 0; i < (WAVELET_SIZE-1); i+=2)
+    {
+        out[i] += (*approx * wavelet[WAVELET_SIZE-1-i]);
+        out[i+1] += (*approx * wavelet[WAVELET_SIZE-2-i]);
+        out[i] -= (*detail * wavelet[i])/2;
+        out[i+1] += (*detail * wavelet[i+1]);
+    }
+}
+
+
